b0b005dfe868c9c6ab25d76d9d61683af0a59764
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 21:58:50 +0700

    add contructor Garments

--- src/model/Garments.java
+++ src/model/Garments.java
@@ -4,4 +4,13 @@ public class Garments {
     private String type;
     private String color;
 
+    public Garments() {
+    }
+
+    public Garments(String type, String color) {
+        this.type = type;
+        this.color = color;
+    }
+
+
 }
