d901c79f092623e5572a4976d4788ff411f4b2a2
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 21:56:16 +0700

    add getter, setter, contructor, toString class HouseholdGoods

--- src/model/HouseholdGoods.java
+++ src/model/HouseholdGoods.java
@@ -1,4 +1,27 @@
 package model;
 
 public class HouseholdGoods {
+    private String manufacturer;
+
+    public HouseholdGoods() {
+    }
+
+    public HouseholdGoods(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "HouseholdGoods{" +
+                "manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
 }
