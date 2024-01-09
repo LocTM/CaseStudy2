419a8698fb500ef3bcb58c8a46e8112631b3ae5d
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 22:12:12 +0700

    add contructor, pthuc add,delete

--- src/controller/ShopManager.java
+++ src/controller/ShopManager.java
@@ -1,4 +1,23 @@
 package controller;
 
+import model.Shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopManager {
+    public static List<Shop> list = new ArrayList<>();
+
+    public void addNew(Shop s){
+        list.add(s);
+    }
+
+    public void deleteById(int index) {
+        list.remove(index);
+    }
+
+    public ShopManager() {
+    }
+
+
 }
