b58da2cf8b1545785e241d4143961e6514ebc888
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 21:44:23 +0700

    add contructor Shop

--- src/model/Shop.java
+++ src/model/Shop.java
@@ -6,4 +6,14 @@ public class Shop {
     private double cost;
     private int year;
 
+    public Shop() {
+    }
+
+    public Shop(String id, String name, double cost, int year) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.year = year;
+    }
+
 }
