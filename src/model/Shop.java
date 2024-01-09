e0c54a50e5d2cfbe3bc9cc495e2edc865d347a48
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 21:44:50 +0700

    add getter, setter

--- src/model/Shop.java
+++ src/model/Shop.java
@@ -16,4 +16,35 @@ public class Shop {
         this.year = year;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
