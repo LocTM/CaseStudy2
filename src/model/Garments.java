4012e1111013227c22deeb007590dcfcdab70edb
LocTM <tranminhloc.anhk@gmail.com>
2024-01-09 21:59:25 +0700

    add getter setter Garments

--- src/model/Garments.java
+++ src/model/Garments.java
@@ -12,5 +12,19 @@ public class Garments {
         this.color = color;
     }
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
