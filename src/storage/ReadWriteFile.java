062dce04a3da64c0e59fc548dc34491c7205f8a4
LocTM <tranminhloc.anhk@gmail.com>
2024-01-10 12:55:25 +0700

    doc file

--- src/storage/ReadWriteFile.java
+++ src/storage/ReadWriteFile.java
@@ -1,6 +1,27 @@
 package storage;
 
-public class ReadWriteFIle {
-    //doc file
+import model.Shop;
 
+import java.io.*;
+import java.util.List;
+
+public class ReadWriteFile {
+    //doc file
+    public List<Shop> readFile(){
+        File file = new File("Shop.txt");
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(inputStream);
+            List<Shop> list = null;
+            list = (List<Shop>) ois.readObject();
+            return list;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
